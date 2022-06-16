f1d95d50393bd4a3c624e9c9d58b9eee02d4bd3c
Fernando Elizalde <fernando.elizalde@revolut.com>
2022-06-16 15:31:10 -0500

    Dont modify JS links

--- linker.go
+++ linker.go
@@ -8,17 +8,14 @@ import (
 // A Linker is used to correct the links found in the response body
 // It will add the prefix when a regex match is found
 type Linker struct {
-	quoted_reg   *regexp.Regexp
-	hue_base_url *regexp.Regexp
+	quoted_reg *regexp.Regexp
 }
 
 func new_linker(paths string) Linker {
 	quoted := `([='"])(` + paths + ")"
-	hue_base := `window.HUE_BASE_URL\s*[+]\s*'/hue'`
 
 	linker := Linker{
-		quoted_reg:   regexp.MustCompile(quoted),
-		hue_base_url: regexp.MustCompile(hue_base),
+		quoted_reg: regexp.MustCompile(quoted),
 	}
 
 	return linker
@@ -27,8 +24,6 @@ func new_linker(paths string) Linker {
 func (l *Linker) replace(file []byte, service_prefix string) []byte {
 	prefix := "$1" + strings.TrimSuffix(service_prefix, "/") + "$2"
 	replaced := l.quoted_reg.ReplaceAll(file, []byte(prefix))
-
-	replaced = l.hue_base_url.ReplaceAll(replaced, []byte("window.HUE_BASE_URL"))
 
 	return replaced
 }
