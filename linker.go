f9b2f86d41ee680986f9e2ae69aef8d4ef245877
Fernando Elizalde <fernando.elizalde@revolut.com>
2022-06-16 15:44:52 -0500

    modify prefix

--- linker.go
+++ linker.go
@@ -8,14 +8,20 @@ import (
 // A Linker is used to correct the links found in the response body
 // It will add the prefix when a regex match is found
 type Linker struct {
-	quoted_reg *regexp.Regexp
+	quoted_reg   *regexp.Regexp
+	hue_base_url *regexp.Regexp
+	prefix_link  *regexp.Regexp
 }
 
 func new_linker(paths string) Linker {
 	quoted := `([='"])(` + paths + ")"
+	hue_base := `window.HUE_BASE_URL\s*[+]\s*'/hue'`
+	prefix_link := `var prefix = '/hue';`
 
 	linker := Linker{
-		quoted_reg: regexp.MustCompile(quoted),
+		quoted_reg:   regexp.MustCompile(quoted),
+		hue_base_url: regexp.MustCompile(hue_base),
+		prefix_link:  regexp.MustCompile(prefix_link),
 	}
 
 	return linker
@@ -24,6 +30,8 @@ func new_linker(paths string) Linker {
 func (l *Linker) replace(file []byte, service_prefix string) []byte {
 	prefix := "$1" + strings.TrimSuffix(service_prefix, "/") + "$2"
 	replaced := l.quoted_reg.ReplaceAll(file, []byte(prefix))
+	replaced = l.hue_base_url.ReplaceAll(replaced, []byte("window.HUE_BASE_URL"))
+	replaced = l.prefix_link.ReplaceAll(replaced, []byte("var prefix = '';"))
 
 	return replaced
 }
--- linker_test.go
+++ linker_test.go
@@ -26,6 +26,22 @@ func TestLinker(t *testing.T) {
 			`<link href="/user/user@comp/static/desktop/css/roboto.895233d7bf84.css" rel="stylesheet">
 			<link href="/user/user@comp/static/desktop/ext/css/font-awesome.min.bf0c425cdb73.css" rel="stylesheet">`,
 		},
+		{
+			`link = window.HUE_BASE_URL + '/hue' + link;`,
+			`link = window.HUE_BASE_URL + link;`,
+		},
+		{
+			`link = window.HUE_BASE_URL+'/hue'+link;`,
+			`link = window.HUE_BASE_URL+link;`,
+		},
+		{
+			`page__WEBPACK_IMPORTED_MODULE_3___default().base(window.HUE_BASE_URL + '/hue');`,
+			`page__WEBPACK_IMPORTED_MODULE_3___default().base(window.HUE_BASE_URL);`,
+		},
+		{
+			`var prefix = '/hue';`,
+			`var prefix = '';`,
+		},
 	}
 
 	for _, tc := range cases {
